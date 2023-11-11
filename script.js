e104d6d968efa9ecab5d9c3f074a7040f9624dd3
Walid MAD <walid.madad123@gmail.com>
2023-11-11 01:25:05 +0100

    update index.html and style.css

--- script.js
+++ script.js
@@ -13,8 +13,32 @@ function nextSlide() {
     showSlide(currentIndex);
 }
 
-
 setInterval(nextSlide, 3000);
 
-
 showSlide(currentIndex);
+
+
+
+
+let currentIndex2 = 0;
+const temoignages = document.querySelectorAll('.temoignage');
+
+function showTemoignage(index) {
+            temoignages.forEach((temoignage, i) => {
+                temoignage.style.display = i === index ? 'block' : 'none';
+            });
+        }
+
+        function prevTemoignage() {
+            currentIndex = (currentIndex - 1 + temoignages.length) % temoignages.length;
+            showTemoignage(currentIndex);
+        }
+
+        function nextTemoignage() {
+            currentIndex = (currentIndex + 1) % temoignages.length;
+            showTemoignage(currentIndex);
+        }
+
+        // Afficher le premier témoignage au chargement de la page
+        showTemoignage(currentIndex);
+    
