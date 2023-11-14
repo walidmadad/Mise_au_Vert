262dd700aac2f2f6799ec0a25fb87ae9c569aeb0
walidmadad <walid.madad123@gmail.com>
2023-11-14 23:28:41 +0100

    update

--- espaceClient.php
+++ espaceClient.php
@@ -28,7 +28,8 @@
                             <li><a href="">Historique</a></li><br><br>
                             <li><a href="">Réservation</a></li><br><br>
                             <li><a href="">Paramétres</a></li><br><br>
-                            <li><a href="#">Se déconecter</a></li><br>
+                            <li><a href="#">Sécurité et Confidentialité</a></li><br><br>
+                            <li><a href="">Se déconecter</a></li>
                         </ul>
                     </div>
                 </div>
@@ -36,6 +37,31 @@
         </section>
         <section class="espaceClient-1">
             <h1>Espace Client</h1><hr>
+            <div class="container_espaceClient2">
+                <div class="side-by-side">
+                    <div class="events">
+                       <h2>Événements à Venir</h2>
+                        <p>Découvrez nos prochains événements spéciaux et journées portes ouvertes. Rejoignez-nous pour des moments mémorables avec nos pensionnaires.</p>
+                        <a href="#upcoming-events">En savoir plus sur nos événements</a>
+                   </div>
+
+                    <div class="customer-support">
+                        <h2>Soutien Client</h2>
+                        <p>Notre équipe de support client est là pour répondre à vos questions et vous aider dans le processus de réservation. Contactez-nous à tout moment pour une assistance rapide.</p>
+                        <a href="#contact-support">Contacter le support client</a>
+                    </div>
+                    <div class="side-by-side">
+                </div>
+            </div>
+            <div class="container_espaceClient2">
+                <div class="side-by-side">
+                    <div class="partnerships">
+                        <h2>Partenaires ou Collaborations</h2>
+                        <p>Découvrez nos partenaires et collaborations qui contribuent à offrir les meilleurs services possibles à nos clients et leurs animaux de compagnie.</p>
+                        <a href="#partnerships">En savoir plus sur nos partenaires</a>
+                    </div>
+                </div>
+            </div>
         </section>
         <script src="script.js"></script>
     </body>
