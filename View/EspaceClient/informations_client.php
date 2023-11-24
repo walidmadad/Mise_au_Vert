9cf21c7239481de90a6d82ff15210b2aa95eb3ab
walidmadad <walid.madad123@gmail.com>
2023-11-24 12:05:18 +0100

    Updates

--- View/EspaceClient/informations_client.php
+++ View/EspaceClient/informations_client.php
@@ -40,17 +40,74 @@
         <section class="espaceClient-1">
             <h1>Gestion de profile</h1><hr>
             <a>Nom: </a><input type"text" id="gestion-profile_nom" name="nom" value="<?php
-
             include_once(realpath(__DIR__ . '/../../Modele/ScriptEspaceClient.php'));
             $scriptEspaceClient = new ScriptEspaceClient();
             echo $scriptEspaceClient->getNom();
             ?>"> <br>
-            <a>Prenom: </a><input type"text" id="gestion-profile_nom" name="nom" value="<?php
-
+            <a>Prenom: </a><input type"text" id="gestion-profile_nom" name="prenomnom" value="<?php
             include_once(realpath(__DIR__ . '/../../Modele/ScriptEspaceClient.php'));
             $scriptEspaceClient = new ScriptEspaceClient();
             echo $scriptEspaceClient->getPrenom();
-            ?>">
+            ?>"> <br>
+            <a>Date de Naissance: </a>
+            <label for="day">Jour:</label>
+            <select id="day" name="day">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+                <option value="11">11</option>
+                <option value="12">12</option>
+                <option value="13">13</option>
+                <option value="14">14</option>
+                <option value="15">15</option>
+                <option value="16">16</option>
+                <option value="17">17</option>
+                <option value="18">18</option>
+                <option value="19">19</option>
+                <option value="20">20</option>
+                <option value="21">21</option>
+                <option value="22">22</option>
+                <option value="23">23</option>
+                <option value="24">24</option>
+                <option value="25">25</option>
+                <option value="26">26</option>
+                <option value="27">27</option>
+                <option value="28">28</option>
+                <option value="29">29</option>
+                <option value="30">30</option>
+                <option value="31">31</option>
+            </select>
+            <label for="month">Mois:</label>
+            <select id="month" name="month" onchange="updateDays()">
+                <option value="1">Janvier</option>
+                <option value="2">Février</option>
+                <option value="3">Mars</option>
+                <option value="4">Avril</option>
+                <option value="5">Mai</option>
+                <option value="6">Juin</option>
+                <option value="7">Juillet</option>
+                <option value="8">Août</option>
+                <option value="9">Septembre</option>
+                <option value="10">Octobre</option>
+                <option value="11">Novembre</option>
+                <option value="12">Décembre</option>
+            </select>
+            <label for="year">Année:</label>
+            <input type="text1" id="year" name="year" placeholder="YYYY">
+            <br>
+
+            <a>Email : </a><input type="text" id="gestion-profile" name="email" value="<?php
+            include_once(realpath(__DIR__ . '/../../Modele/ScriptEspaceClient.php'));
+            $scriptEspaceClient = new ScriptEspaceClient();
+            echo $scriptEspaceClient->getEmail();
+            ?>"
         </section>
         <script src="script.js"></script>
     </body>
