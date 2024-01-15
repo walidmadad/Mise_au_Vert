a45234984cc3460e18c3b29f28f5ab206c52a9f6
walidmadad <walid.madad123@gmail.com>
2024-01-15 23:38:03 +0100

    Commit

--- View/EspacePension/Lambertsart.php
+++ View/EspacePension/Lambertsart.php
@@ -7,6 +7,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="../../style.css">
     <link rel="icon" type="image/x-icon" href="../../res/logo.png">
+    <style>
+        th, td{
+            border : 2px red solid;
+        }
+    </style>
     <title>Pension Lambertsart</title>
 </head>
 <body>
@@ -32,7 +37,69 @@
         </div>
     </header>
     <main>
-        <section></section>
+        <section id="content">
+            <h2>Nos Services</h2>
+            <p>Nos services incluent le bain, la promenade et le transport. Lors de l'arrivée de votre animal chez nous, son carnet de santé est vérifié...</p>
+
+            <h2>Visite de notre pension</h2>
+            <p>Sans rendez-vous de 8h30 à 12h et de 14h30 à 19h, tous les jours sauf mardi après-midi, dimanche et jours fériés.</p>
+
+            <h2>Nos Formules</h2>
+            <h3>Hôtel Canin</h3>
+            <p>L’hôtel canin, c’est la solution confort pour votre chien ! Il pourra évoluer à sa convenance dans son espace
+                intérieur et sa cour privative.
+                Cet hébergement a été étudié pour convenir à tous types de chien.
+                Le box intérieur chauffé et son parcours extérieur de 15 à 25 m².
+                Cet hébergement est idéal en hiver et pour les séjours court ou
+                longue durée afin de leur permettre de s’abriter après une
+                longue partie de jeu, et se reposer dans un espace « comme à la
+                maison »</p>
+
+            <h3>Camping Canin</h3>
+            <p>Le camping canin se compose de niches individuelles et d’un grand jardin commun où vos chiens pourront
+                se défouler à volonté.
+                L’atout de cet hébergement est la possibilité pour votre chien d’être au contact des autres animaux tout en
+                gardant son espace individuel.</p>
+
+            <h3>Pension Féline</h3>
+            <p>Nos espaces félins sont étudiés pour répondre aux besoins de nos amis chats…
+                Nous leurs avons préparé des petits cocons douillés et chauffés au sein d’un espace de 50m² aménagé
+                comme à la maison avec petit mobilier et décoration.
+                Ils bénéficient de calme et de tranquillité avec une ambiance musicale.
+                Nos amis les chats peuvent se promener le long des différents parcours aménagés : arbres à chat géants,
+                passerelles, ponts, cordes, planches grattoirs.
+                Pour son bien-être toutes les règles d’hygiène sont respectées,
+                il est aussi essentiel de leurs donner une alimentation riche et
+                équilibré. C’est pourquoi, nous sommes partenaires Proplan.</p>
+
+            <h2>Tarifs</h2>
+            <table>
+                <thead>
+                <tr>
+                    <th>Formule</th>
+                    <th>Durée</th>
+                    <th>Tarif</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>Hôtel Canin</td>
+                    <td>Court Séjour</td>
+                    <td>26€ par jour</td>
+                </tr>
+                <tr>
+                    <td>Camping Canin</td>
+                    <td>Court Séjour</td>
+                    <td>12€ par jour</td>
+                </tr>
+                <tr>
+                    <td>Pension Féline</td>
+                    <td>Court Séjour</td>
+                    <td>11€ par jour</td>
+                </tr>
+                </tbody>
+            </table>
+        </section>
     </main>
 </body>
 </html>
